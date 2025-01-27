3e7998389c344f0069163a4dd6968bd63df597af
evangelostsakanikas <evangelostsakanikas@gmail.com>
2025-01-27 12:32:21 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = '901e1228-ac5a-4dbe-95a7-444d778f6970';
+}
